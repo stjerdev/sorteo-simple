b5995fdbae4210c1201c823b12fc4265aacc7384
Zacarias F. Ojeda <zojeda@jusentrerios.gov.ar>
2020-10-20 11:04:46 -0300

    Updates

---
+++ precache-manifest.f8cfe5057658df626048c391dec3ee9b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c619e85ecc0051a1c9ba23e71f1f112f",
+    "url": "/sorteo-simple/index.html"
+  },
+  {
+    "revision": "3c153458294a1086f55e",
+    "url": "/sorteo-simple/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "be81bdc34d3b13550994",
+    "url": "/sorteo-simple/static/js/2.1b76fe62.chunk.js"
+  },
+  {
+    "revision": "7a870419af2a9bfab083c177351477fd",
+    "url": "/sorteo-simple/static/js/2.1b76fe62.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49e56bc22558771c1b0e",
+    "url": "/sorteo-simple/static/js/3.1a843487.chunk.js"
+  },
+  {
+    "revision": "7ec01595672f75e83fd81b41f132f4c1",
+    "url": "/sorteo-simple/static/js/3.1a843487.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0225854313b6538f0131",
+    "url": "/sorteo-simple/static/js/4.6409bc3e.chunk.js"
+  },
+  {
+    "revision": "0512cf1466087f5c44c5c61e4b32a001",
+    "url": "/sorteo-simple/static/js/4.6409bc3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "695edcc5036762a2f46e",
+    "url": "/sorteo-simple/static/js/5.4023de06.chunk.js"
+  },
+  {
+    "revision": "3a7bc1760b9d274e2e5e7531bce86076",
+    "url": "/sorteo-simple/static/js/5.4023de06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c153458294a1086f55e",
+    "url": "/sorteo-simple/static/js/main.3c8bc995.chunk.js"
+  },
+  {
+    "revision": "38c5efaa9d89d2486d50",
+    "url": "/sorteo-simple/static/js/runtime-main.61e3c3b1.js"
+  }
+]);
